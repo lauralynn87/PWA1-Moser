7654e0db38069b6f4e212f0985f47a8358313301
lauralynn87 <lauralynn@Lauras-MacBook-Pro.local>
2014-05-11 21:31:26 -0500

    Goal 1 Commit 1

---
+++ Week 1 /goal1_assign_duel1/js/main.js
@@ -0,0 +1,12 @@
+/*
+Name: Laura Moser
+Date: 5/11/14
+Assignment: Goal1: Duel - The Wicked Witch vs. The Evil Queen (from Once Upon a Time.
+*/
+
+//self-executing fuction
+(function(){
+ 
+ 	console.log("FIGHT!!");
+ 
+})();
