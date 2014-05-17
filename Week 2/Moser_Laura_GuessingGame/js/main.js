c4bf37daaf9ade39a4dadb5c7b07ceb30d1beb19
lauralynn87 <lauralynn@Lauras-MacBook-Pro.local>
2014-05-16 20:11:38 -0500

    Second Commit

--- Week 2/Moser_Laura_GuessingGame/js/main.js
+++ Week 2/Moser_Laura_GuessingGame/js/main.js
@@ -7,8 +7,27 @@
  * GUESSING GAME
  */
 
-//Game variables
+
 (function ()
+
+//Game variables
+var playersGuess = 0;
+var guessesRemaining = 3;
+var guessesMade = 0;
+
+var dom = {
+	input: document.querySelector("#input:"),
+	output: document.querySelector("#output"),
+	button: document.querySelector("button")
+};
+
+var randomNumber = Math.floor((Math.random() * 10) + 1);
+
+var validateInput = function() {
+	playersGuess = parseInt(dom.input.value);
+};
+
+
 
 
 
