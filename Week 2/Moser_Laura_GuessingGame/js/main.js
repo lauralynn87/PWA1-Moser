2914421c6912d8ffbb21bb1ebdbdd77646a5d06f
lauralynn87 <lauralynn@Lauras-MacBook-Pro.local>
2014-05-16 21:40:16 -0500

    fourth commit

--- Week 2/Moser_Laura_GuessingGame/js/main.js
+++ Week 2/Moser_Laura_GuessingGame/js/main.js
@@ -8,16 +8,16 @@
  */
 
 
-(function ()
+(function () {
 
-//Game variables
+//Game variables - These are Global.
 var playersGuess = 0;
 var guessesRemaining = 3;
 var guessesMade = 0;
 
-//DOM Variables
+//DOM Elements Setup in my index.html page
 var dom = {
-	input: document.querySelector("#input:"),
+	input: document.querySelector("#input"),
 	output: document.querySelector("#output"),
 	button: document.querySelector("button")
 };
@@ -31,21 +31,64 @@ console.log("Random Number: "+randomNumber)
 var validateInput = function() {
 	playersGuess = parseInt(dom.input.value);
 	
-	if(isNaN(playerGuess)){
+	if(isNaN(playersGuess)){
 		dom.output.innerHTML = "Please enter a number between 1 and 10";
 		
-	else if (playerGuess < 1 || playerGuess > 10) {
+	}else if (playerGuess < 1 || playersGuess > 10) {
 		dom.out.innerHTML = "Please make sure your guess is between numbers 1 and 10";
-		
-	else {
-		playGame();
+	
+	}else {
+		guessNumber();
 	};
 
 	
 };
 
-dom.button.
 
+//Guessing Game
+var guessNumber = function() {
+	guessRemain--;
+	guessMade++;
+	gameState = "Guess: " + guessMade + " Remaining " + guessRemain;
+	
+	//matches player's guess to random number
+	playersGuess = parseInt(input.value);
+	
+	if(playersGuess > randomNumber){
+		dom.output.innerHTML = "That's to high, try again." + gameState;
+		
+	}else if(playersGuess < randomNumber){
+		dom.output.innerHTML = "That's to low, try again." + gameState;
+
+	}else if (playersGuess === randomNumber) {
+		gameOver(true);
+	}
+};
+
+console.log("Players Guess: " + playersGuess);
+
+//Winner's Circle - Is the Game Over? Who Won? 
+var gameOver = function(win) {
+	
+	if (win){
+		dom.output.innerHTML
+		= "Congratulations! You guessed the right number!" + "</br>"
+		+ "You got it in " + guessMade + " guesses.";
+	}else{
+		dom.output.innerHTML
+		= "Sorry, there are no more guesses left. The number was " + randomNumber;
+		};
+		
+		dom.button.removeEventListener("click",clickFn, false);
+};
+
+//Allows user to use the 'Enter' key instead of click on the button.
+var onKeyDown = function(e) {
+	if(e.keycode === 13){
+	validateInput();
+	};
+
+};
 
 
 
