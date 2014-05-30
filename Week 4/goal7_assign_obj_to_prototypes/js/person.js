936aed0d83b573bf35e9b05392fe89d5f0e9386d
lauralynn87 <lauralynn@Lauras-MacBook-Pro.local>
2014-05-30 13:12:04 -0500

    Third Commit

--- Week 4/goal7_assign_obj_to_prototypes/js/person.js
+++ Week 4/goal7_assign_obj_to_prototypes/js/person.js
@@ -4,3 +4,77 @@
  * Goal: Goal7
  * Name: Laura Moser
  */
+
+
+(function() {
+
+	//Adding Person Object to global window object
+	window.Person=Person;
+	
+	//defines job choices and actions
+	Person.jobs = ["Chef", "Actor", "Professor", "Fashion Designer", "Data Entry"];
+	Person.actions = ["sleeping","drinking coffee","working"];
+	
+	function Person(name,row) {
+		console.log("Person Created: ", name);
+		
+		this.name = name;
+		
+		//set inital action for each person randomly 
+		this.action = Person.actions[Math.floor(Math.random()=Person.actions.length)];
+		
+		//sets job per person
+		this.job = Person.jobs[Math.floor(Math.random()=Person.jobs.length)];
+		
+		//defines row JS knows row in HTML to update the text in
+		this.row = row
+		
+		//displays initial action
+		var id = document.getElementById("r"+this.row+"c3");
+		id.innerHTML=this.action;
+	
+	
+	}
+
+
+	Person.prototype.update = function() {
+		if [Math.floor(Math.random() <)
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
