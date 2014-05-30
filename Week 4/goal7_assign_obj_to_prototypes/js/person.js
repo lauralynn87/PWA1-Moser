569a67d964f5e774f719f831c93080fe0c80762c
lauralynn87 <lauralynn@Lauras-MacBook-Pro.local>
2014-05-30 13:21:36 -0500

    Sixth Commit

--- Week 4/goal7_assign_obj_to_prototypes/js/person.js
+++ Week 4/goal7_assign_obj_to_prototypes/js/person.js
@@ -37,16 +37,13 @@
 	}
 
 
-	Person.prototype.update = function() {
-		if [Math.floor(Math.random() <.01)) {
+	Person.prototype.update = function(){
+		if(Math.floor(Math.random() < .01)){
 			var i = Math.floor(Math.random() * Person.actions.length);
-			this.action = Person.action[i];
-			var id = document.getElementById("r" + this.row + "c3");
+			this.action = Person.actions[i];
+			var id = document.getElementById("r" +this.row+"c3");
 			id.innerHTML = this.action;
 		}
-	
-	
-	
 	}
 
 
